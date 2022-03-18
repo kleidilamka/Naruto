133b77bc70144b797ec44cb51c4890fc535809f7
kleidilamka <kleidilamka@gmail.com>
2022-03-18 17:17:42 +0100

    b1

---
+++ a1.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+export default function a1() {
+  return (
+    <View>
+      <Text>a1</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
