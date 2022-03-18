82e52a758b23aa198c031f69a379562eb0704d0f
kleidilamka <kleidilamka@gmail.com>
2022-03-18 17:57:12 +0100

    salsa

--- a1.tsx
+++ a1.tsx
@@ -1,11 +1,7 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 
-<<<<<<< HEAD
 export default function a1({ props23 }) {
-=======
-export default function a1({ props3 }) {
->>>>>>> tao
   return (
     <View>
       <Text>a1</Text>
