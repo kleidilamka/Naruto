871f2e9ef06dea85cd31cad852c017998823a4dd
kleidilamka <kleidilamka@gmail.com>
2022-03-18 17:21:48 +0100

    tao

--- a1.tsx
+++ a1.tsx
@@ -1,11 +1,7 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 
-<<<<<<< HEAD
-export default function a1(props) {
-=======
 export default function a1({ props }) {
->>>>>>> mao
   return (
     <View>
       <Text>a1</Text>
